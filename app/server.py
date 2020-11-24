76a0bc65b956869f362fc8ea270b7958ec9a8762
luanfrinhani <49029493+luanfrinhani@users.noreply.github.com>
2020-11-24 13:56:45 -0300

    Update server.py

--- app/server.py
+++ app/server.py
@@ -48,7 +48,7 @@ async def setup_learner():
 
 loop = asyncio.get_event_loop()
 tasks = [asyncio.ensure_future(setup_learner())]
-
+learn = loop.run_until_complete(asyncio.gather(*tasks))[0]
 loop.close()
 
 
