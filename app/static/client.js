e79dd2afc49fd3c1634774ae204b3c16797a3cba
luanfrinhani <49029493+luanfrinhani@users.noreply.github.com>
2020-11-30 19:08:55 -0300

    Update client.js

--- app/static/client.js
+++ app/static/client.js
@@ -29,7 +29,7 @@ function analyze() {
   xhr.onload = function(e) {
     if (this.readyState === 4) {
       var response = JSON.parse(e.target.responseText);
-      el("result-label").innerHTML = `Resultado = ${response["result"]}`;
+      el("result-image").innerHTML = `Resultado = ${response["result"]}`;
     }
     el("analyze-button").innerHTML = "Classificar";
   };
