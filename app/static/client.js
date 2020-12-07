43d3c1eba754e9d43f6b127fa012051fbe20904d
luanfrinhani <49029493+luanfrinhani@users.noreply.github.com>
2020-12-07 14:42:51 -0300

    Update client.js

--- app/static/client.js
+++ app/static/client.js
@@ -16,9 +16,9 @@ function showPicked(input) {
 
 function analyze() {
   var uploadFiles = el("file-input").files;
-  if (uploadFiles.length !== 1) alert("Por favor selecione um arquivo");
+  if (uploadFiles.length !== 1) alert("Please select a file to analyze!");
 
-  el("analyze-button").innerHTML = "Classificando...";
+  el("analyze-button").innerHTML = "Analyzing...";
   var xhr = new XMLHttpRequest();
   var loc = window.location;
   xhr.open("POST", `${loc.protocol}//${loc.hostname}:${loc.port}/analyze`,
@@ -38,4 +38,3 @@ function analyze() {
   fileData.append("file", uploadFiles[0]);
   xhr.send(fileData);
 }
-
