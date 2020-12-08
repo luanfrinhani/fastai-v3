5df53c09e826f1e1f11128bc8d8f95e0cde5625c
luanfrinhani <49029493+luanfrinhani@users.noreply.github.com>
2020-12-07 22:51:47 -0300

    Update server.py

--- app/server.py
+++ app/server.py
@@ -12,8 +12,8 @@ import numpy as np
 
 
 
-export_file_url = 'https://www.dropbox.com/s/tluwr2ad2x51uj0/Segmantation-1-30-11.pkl?dl=1'
-export_file_name = 'Segmantation-1-30-11.pkl'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1-4yqTZoce96sylXTzYz0w_N32ZqU2fFy'
+export_file_name = 'Tentativa-2-96.pth'
 
 codes = np.array(['background','caixa_dagua_aberta', 'caixa_dagua_fechada', 'piscina_suja', 'piscina_limpa', 'agua_parada', 'padder1', 'pneu'])
 classes = codes
