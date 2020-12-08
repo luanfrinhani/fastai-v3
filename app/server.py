062c9d090d1adf068d3f1a3d3c93c4dfa8e874d1
luanfrinhani <49029493+luanfrinhani@users.noreply.github.com>
2020-12-07 21:03:07 -0300

    Update server.py

--- app/server.py
+++ app/server.py
@@ -13,7 +13,7 @@ import numpy as np
 export_file_url = 'https://www.dropbox.com/s/tluwr2ad2x51uj0/Segmantation-1-30-11.pkl?dl=1'
 export_file_name = 'Segmantation-1-30-11.pkl'
 
-codes = np.array(['background','caixa_dagua_aberta', 'caixa_dagua_fechada', 'piscina_suja', 'piscina_limpa', 'agua_parada', 'padder1', 'pneu'])
+#codes = np.array(['background','caixa_dagua_aberta', 'caixa_dagua_fechada', 'piscina_suja', 'piscina_limpa', 'agua_parada', 'padder1', 'pneu'])
 path = Path(__file__).parent
 
 app = Starlette()
