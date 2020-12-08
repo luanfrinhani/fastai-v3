441a406818430decec522a8b2530709665eaa408
luanfrinhani <49029493+luanfrinhani@users.noreply.github.com>
2020-12-07 22:01:11 -0300

    Update server.py

--- app/server.py
+++ app/server.py
@@ -10,10 +10,12 @@ from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 import numpy as np
 
+defaults.device = torch.device('cpu')
 export_file_url = 'https://www.dropbox.com/s/tluwr2ad2x51uj0/Segmantation-1-30-11.pkl?dl=1'
 export_file_name = 'Segmantation-1-30-11.pkl'
 
-#codes = np.array(['background','caixa_dagua_aberta', 'caixa_dagua_fechada', 'piscina_suja', 'piscina_limpa', 'agua_parada', 'padder1', 'pneu'])
+codes = np.array(['background','caixa_dagua_aberta', 'caixa_dagua_fechada', 'piscina_suja', 'piscina_limpa', 'agua_parada', 'padder1', 'pneu'])
+classes = codes
 path = Path(__file__).parent
 
 app = Starlette()
